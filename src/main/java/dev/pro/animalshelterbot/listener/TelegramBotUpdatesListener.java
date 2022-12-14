31c6866a8ede0bd1b746c4005b6ebdc23a1792e0
NIKO <nzubovskiy@gmail.com>
2022-12-14 20:03:41 +0400

    Commit 15/12/22

--- src/main/java/dev/pro/animalshelterbot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/dev/pro/animalshelterbot/listener/TelegramBotUpdatesListener.java
@@ -37,6 +37,8 @@ import java.util.List;
 import java.util.Optional;
 
 
+import static dev.pro.animalshelterbot.constants.BotStatus.KEEPING_a_PET;
+
 /**
  * The main service of the bot containing the logic of processing incoming updates
  */
