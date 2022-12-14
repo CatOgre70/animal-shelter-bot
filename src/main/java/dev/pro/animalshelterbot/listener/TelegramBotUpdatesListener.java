325fee3b99bd8435fb51170c87e75191ec1ba1d0
Vasily Demin <vasily.demin@gmail.com>
2022-12-14 20:17:45 +0300

    Added photo uploading and downloading for DailyReportController, added liqiubase sql scripts added test data into daily_report table, some minor changes in other files

--- src/main/java/dev/pro/animalshelterbot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/dev/pro/animalshelterbot/listener/TelegramBotUpdatesListener.java
@@ -7,11 +7,8 @@ import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
-import dev.pro.animalshelterbot.constants.BotStatus;
-import dev.pro.animalshelterbot.constants.Commands;
-import dev.pro.animalshelterbot.constants.Constants;
 import dev.pro.animalshelterbot.factory.KeyboardFactory;
-import dev.pro.animalshelterbot.constants.Constants;
+import dev.pro.animalshelterbot.constants.*;
 import dev.pro.animalshelterbot.model.ChatConfig;
 import dev.pro.animalshelterbot.model.User;
 import dev.pro.animalshelterbot.service.ChatConfigService;
@@ -22,8 +19,6 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
-
-import static dev.pro.animalshelterbot.constants.BotStatus.KEEPING_a_PET;
 
 /**
  * The main service of the bot containing the logic of processing incoming updates
@@ -55,79 +50,137 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     @Override
     public int process(List<Update> updates) {
         updates.forEach(update -> {
-                    logger.info("Processing update: {}", update);
+            // Process your updates here
+            logger.info("Processing update: {}", update);
+            Long chatId = 0L;
+            BotStatus botStatus;
 
-                    // Process your updates here
+            UpdateType updateType = checkingUpdate(update);
+            if(updateType == UpdateType.COMMAND || updateType == UpdateType.MESSAGE || updateType == UpdateType.PHOTO) {
+                chatId = update.message().chat().id();
+            } else if(updateType == UpdateType.CALL_BACK_QUERY) {
+                chatId = update.callbackQuery().from().id();
+            }
 
+            ChatConfig chatConfig = chatConfigService.findByChatId(chatId);
 
-            checkingUpdate(update);
-            checkStartMessage(update);
-            statusDeterminant(update.message().chat().id(), update);
+            if(chatConfig == null) { // New user
+                chatConfig = new ChatConfig(chatId, BotStatus.DEFAULT.status);
+                chatConfigService.addChatConfig(chatConfig);
+                botStatus = BotStatus.NEW_USER;
+            } else { // Not new user
+                botStatus = BotStatus.valueOfStatus(chatConfig.getChatState());
+            }
+
+            switch (updateType) {
+                case COMMAND:
+                    processCommands(update, botStatus);
+                    break;
+                case MESSAGE:
+//                    processMessages(update, botStatus);
+                    break;
+                case PHOTO:
+//                    processPhotos(update, botStatus);
+                    break;
+                case CALL_BACK_QUERY:
+//                    processCallbackQueries(update, botStatus);
+                    break;
+                case ERROR:
+//                    processErrors(update, botStatus);
+                    break;
+            }
+
+//            statusDeterminant(update.message().chat().id(), update);
 
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
 
-    private void checkStartMessage(Update update) {
-        String message = Constants.REQUEST_START;
+    private void processCommands(Update update, BotStatus botStatus) {
         Long chatId = update.message().chat().id();
-        String incomingMessage = update.message().text();
+        Commands command = Commands.valueOfCommandText(update.message().text());
+        if(command == null) {
+            sendMessage(chatId, Messages.THERE_IS_NO_SUCH_COMMAND.messageText);
+            sendMessage(chatId, Messages.HELP.messageText);
+            return;
+        }
+        if(command == Commands.START) {
+            switch (botStatus) {
+                case NEW_USER:
+                    sendMessage(chatId, Messages.WELCOME_TO_THE_CHATBOT.messageText);
+                    sendMessage(chatId, Messages.HELP.messageText);
+                    break;
+                case DEFAULT:
+                case CONSULT_NEW_USER:
+                case CONSULT_POTENTIAL_OWNER:
+                case KEEPING_a_PET:
+                case CHAT_WITH_VOLUNTEER:
+                    sendMessage(chatId, Messages.HELP.messageText);
+            }
+        } else if(command == Commands.HELP) {
+            sendMessage(chatId, Messages.HELP.messageText);
+        } else if(command == Commands.MENU) {
+            sendMessage(chatId, "Тут будет контекстное меню, а также могла бы быть ваша реклама :)");
+        }
 
-        if (!userService.checkByChatId(chatId)) {
-            if (findCommandByString(incomingMessage) == Commands.START) {
+//        if (!userService.checkByChatId(chatId)) {
+//            if (findCommandByString(incomingMessage) == Commands.START) {
 //                message = new SendMessage(chatId, Constants.REQUEST_START);
-                userService.addUser(new User(update.message().chat().firstName(), update.message().chat().lastName(), update.message().chat().username(), null, null, update.message().chat().id()));
-                chatConfigService.addChatConfig(new ChatConfig(chatId, 0L));
-                SendMessage sendMessage = new SendMessage(chatId, Constants.START_DESCRIPTION);
-                SendResponse sendResponse = telegramBot.execute(sendMessage);
- // это кинуть в отдельный метод, т.к. в этом не вызывается или порешать с ошибкой
-                // SendMessage keys = new SendMessage(update.message().chat().id(), Constants.CHOOSE_OPTION);
- //               keys.replyMarkup(KeyboardFactory.startButtons());
- //               SendResponse response1 = telegramBot.execute(keys);
-            } else if (!incomingMessage.equalsIgnoreCase("/start")) {
-                SendMessage sendMessage = new SendMessage(chatId, "Вы ошиблись с вводом команды. " + Constants.REQUEST_START);
-                SendResponse sendResponse = telegramBot.execute(sendMessage);
-            }
-        } else statusDeterminant(chatId, update);
+//                userService.addUser(new User(update.message().chat().firstName(), update.message().chat().lastName(), update.message().chat().username(), null, null, update.message().chat().id()));
+//                chatConfigService.addChatConfig(new ChatConfig(chatId, 0L));
+//                SendMessage sendMessage = new SendMessage(chatId, Constants.START_DESCRIPTION);
+//                SendResponse sendResponse = telegramBot.execute(sendMessage); //это кинуть в отдельный метод, т.к. в этом не вызывается или порешать с ошибкой
+//
+//                SendMessage keys = new SendMessage(update.message().chat().id(), Constants.CHOOSE_OPTION);
+//                keys.replyMarkup(KeyboardFactory.startButtons());
+//                SendResponse response1 = telegramBot.execute(keys);
+//            } else if (!incomingMessage.equalsIgnoreCase("/start")) {
+//                SendMessage sendMessage = new SendMessage(chatId, "Вы ошиблись с вводом команды. " + Constants.REQUEST_START);
+//                SendResponse sendResponse = telegramBot.execute(sendMessage);
+//            }
+//        } else statusDeterminant(chatId, update);
     }
 
-    private Commands findCommandByString(String incomingMessage) {
-        for (Commands c : Commands.values()) {
-            if(c.equals(incomingMessage)) {
-                return c;
-            }
-        }
-        return Commands.HELP;
-    }
 
-    private void checkingUpdate(Update update) {
+    private UpdateType checkingUpdate(Update update) {
         if (update.message() != null) {
-            String incomingMessage = update.message().text();
-            Long chatId = update.message().chat().id();
-        } else if (update.message().photo() != null) {
-            PhotoSize[] incomingMessage = update.message().photo();
-            Long chatId = update.message().chat().id();
-            if (!userService.checkByChatId(chatId)) {
-                SendMessage message = new SendMessage(chatId, "Вы ошиблись с вводом команды. " + Constants.REQUEST_START);
-                SendResponse response = telegramBot.execute(message);
-            } else if (chatConfigService.findByChatId(chatId).equals(Constants.KEEPING_a_PET)) {
-                // проверка наличия текста DailyReport, если есть, то ок, если нет - запрос
+            if(update.message().text() != null) {
+                if (update.message().text().startsWith("/")) {
+                    return UpdateType.COMMAND;
+                } else {
+                    return UpdateType.MESSAGE;
+                }
+            } else if (update.message().photo() != null) {
+                return UpdateType.PHOTO;
             } else {
-                SendMessage message = new SendMessage(chatId, "У Вас нет животного на адаптации, вы не можете направить фотоотчёт." + Constants.CHOOSE_OPTION + Commands.MENU);
-                SendResponse response = telegramBot.execute(message);
+                return UpdateType.ERROR;
             }
+
+//            PhotoSize[] incomingMessage = update.message().photo();
+//            Long chatId = update.message().chat().id();
+//            if (!userService.checkByChatId(chatId)) {
+//                SendMessage message = new SendMessage(chatId, "Вы ошиблись с вводом команды. " + Constants.REQUEST_START);
+//                SendResponse response = telegramBot.execute(message);
+//            } else if (chatConfigService.findByChatId(chatId).equals(Constants.KEEPING_a_PET)) {
+//                // проверка наличия текста DailyReport, если есть, то ок, если нет - запрос
+//            } else {
+//                SendMessage message = new SendMessage(chatId, "У Вас нет животного на адаптации, вы не можете направить фотоотчёт." + Constants.CHOOSE_OPTION + Commands.MENU);
+//                SendResponse response = telegramBot.execute(message);
+//            }
         } else if (update.callbackQuery() != null) { // Callback answer processing
-            Long chatId = update.message().chat().id();
-            SendMessage message = new SendMessage(chatId, "Вы ошиблись с вводом команды. " + Constants.REQUEST_START);
-            SendResponse response = telegramBot.execute(message);
+            return UpdateType.CALL_BACK_QUERY;
+//            Long chatId = update.message().chat().id();
+//            SendMessage message = new SendMessage(chatId, "Вы ошиблись с вводом команды. " + Constants.REQUEST_START);
+//            SendResponse response = telegramBot.execute(message);
 
+        } else {
+            return UpdateType.ERROR;
         }
-
     }
 
     private SendResponse statusDeterminant(Long chatId, Update update) {
-        Long botStatus = chatConfigService.findByChatId(chatId);
+        Long botStatus = chatConfigService.findByChatId(chatId).getChatId();
         SendMessage message = new SendMessage(chatId, Constants.CHOOSE_OPTION);
         SendResponse response = telegramBot.execute(message);
         BotStatus bStatus = getBotStatusByLong(botStatus);
@@ -179,8 +232,7 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         KeyboardFactory.startButtons();
     }
 
-    private void sendMessage(Update update, String message) {
-        Long chatId = update.message().chat().id();
+    private void sendMessage(Long chatId, String message) {
         SendMessage sendMessage = new SendMessage(chatId, message);
         SendResponse response = telegramBot.execute(sendMessage);
         if (response.isOk()) {
